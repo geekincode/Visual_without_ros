0ae828cefc47812b16b1dcebc1eea2de73d67420
Your Name <you@example.com>
2026-03-05 05:05:03 +0800

    Updates

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// NOTE: If you will host at https://<username>.github.io/Visual_without_ros,
-// set base to '/Visual_without_ros/'. If you host at a user/site root page, set base to '/'.
-export default defineConfig({
-  base: '/Visual_without_ros/',
-  plugins: [react()]
-})
